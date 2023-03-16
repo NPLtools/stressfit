pre-release fixes, adds 2nd derivative smoothing as an option

stressfit/mccfit.py
@@ -25,6 +25,12 @@ import stressfit.sample as sam
 import stressfit.graphs as gr
 import stressfit.dataio as dataio
 
+
+# version of regularization function
+#_reg_version = 1 # minimizes 1st derivative
+_reg_version = 2 # minimizes 2nd derivative
+
+
 _chi2 = np.inf
 _reg = np.inf
 _ispline = None # spline interpolator for intensity, used by SFit
@@ -280,7 +286,9 @@ def fitcallb(params, iteration, resid, *fcn_args, **fcn_kws):
 
 # define objective function for fitting: returns the array to be minimized
 def costFnc(params, model, fitstat):
-    """Residual array to be passed to the minimizer: intensity scan."""
+    """Residual array to be passed to the minimizer."""
+    if _reg_version==2:
+        return costFnc2(params, model, fitstat)
     model.params = params
     res = model.calResid(guess=fitstat.guess)
     nres = res.shape[0]
@@ -290,6 +298,31 @@ def costFnc(params, model, fitstat):
     if (areg > 0):
         # model.updateDistribution()
         der = model.calDeriv()
+        reg =np.sqrt(areg)*np.absolute(der)
+        nreg = reg.shape[0]       
+        w = np.sqrt(1.0*abs(nres + nreg - model.nvar)/abs(nres - model.nvar))
+        res = np.concatenate((res, reg), axis=0)*w
+    if (model.constraint is not None):
+        w = np.sqrt(1.0*abs(nres + nreg - model.nvar))
+        y = w*model.constraint(params)
+        res = np.concatenate((res, np.array([y])), axis=0)
+    return res
+
+
+def costFnc2(params, model, fitstat):
+    """Residual array to be passed to the minimizer.
+    
+    version 2 with 2nd derivative regularization.
+    """
+    model.params = params
+    res = model.calResid(guess=fitstat.guess)
+    nres = res.shape[0]
+    nreg = 0
+    # add regularization term
+    areg = fitstat.areg
+    if (areg > 0):
+        # model.updateDistribution()
+        der = model.calDeriv2()
         reg =np.sqrt(areg)*np.absolute(der)
         nreg = reg.shape[0]       
         w = np.sqrt(1.0*abs(nres + nreg - model.nvar)/abs(nres - model.nvar))
@@ -813,6 +846,20 @@ class MCCfit(ABC):
             res[i] = dy/dx
         return res
 
+    def calDeriv2(self):
+        """Calculate 2nd derivative of the distribution."""
+        nx = self.dist.shape[0]
+        res = np.zeros(nx-2)
+        x = self.dist[:,0]
+        y = self.dist[:,1]
+        for i in range(nx-2):
+            dx1 = x[i+1] - x[i]
+            dx2 = x[i+2] - x[i+1]
+            dy1 = y[i+1] - y[i] 
+            dy2 = y[i+2] - y[i+1]
+            res[i] = 2*(dy1/dx1 - dy2/dx2)/(dx1+dx2)
+        return res
+    
     def formatResultModel(self):
         """Format a table with deconvoluted curves.
         
@@ -1061,9 +1108,7 @@ class MCCfit(ABC):
             rec['loop'] = loop
         if ireg:
             rec['ireg'] = loop
-        return rec
-    
-             
+        return rec             
     
     def saveResults(self, outpath, fname, reglog=None):
         """Save fit results.
@@ -1083,7 +1128,6 @@ class MCCfit(ABC):
         if (hasData and hasFit and fname):  
             f = dataio.derive_filename(fname, ext='dat', sfx='model')
             fn = str(dataio.get_output_file(f, path=outpath))
-            # fn = deriveFilename(outpath+fname, ext='dat', sfx='model')
             ss = self.formatResultModel()
             print('Model saved in '+fn)
             with open(fn, 'w') as f:
@@ -1093,7 +1137,6 @@ class MCCfit(ABC):
             # Save original data and fit 
             f = dataio.derive_filename(fname, ext='dat', sfx='fit')
             fn = str(dataio.get_output_file(f, path=outpath))
-            #fn = deriveFilename(outpath+fname, ext='dat', sfx='fit')
             ss = self.formatResultFit()
             print('Fit saved in '+fn)
             with open(fn, 'w') as f:
@@ -1104,7 +1147,6 @@ class MCCfit(ABC):
             # Save log with parameters 
             f = dataio.derive_filename(fname, ext='log', sfx='')
             fn = str(dataio.get_output_file(f, path=outpath))
-            # fn = deriveFilename(outpath+fname, ext='log', sfx='')
             ss = self.formatResultLog()
             
             if (reglog is not None):
@@ -1436,6 +1478,63 @@ class Sfit(MCCfit):
         y = A*res + B
         ey = A*err
         return [y, ey, pos]
+
+
+    def formatIntensityModel(self):
+        """Format a table with intensity model attached to the strain model.
+        
+        If there is no intensity model defined, or is constant, return emty string.
+        
+        Returns
+        -------
+            String
+                tab-delimited table
+        """
+        has_it = (_ispline is not None) and (self.dist is not None)
+        if has_it:
+            x = self.dist[:,0]
+            y = self.intFnc(x)
+            y0 = y[0]
+            dy = y - y0
+            has_it = dy.any()
+        if not has_it:
+            return ''        
+        ss = "# Intensity model for strain fit\n"    
+        ss += "# Date: " + str(datetime.datetime.now()) + "\n"
+        hdr = ['xi', 'yi']
+        ss += '# Header: ' + '\t'.join(f for f in hdr) + '\n'
+        fmt = '{:g}\t{:g}\n'
+        for i in range(len(x)):
+            ss += fmt.format(x[i], y[i])
+        return ss
+
+    def saveResults(self, outpath, fname, reglog=None):
+        """Save fit results.
+        
+        Parameters
+        ----------
+        outpath : str
+            Output path.
+        fname : str
+            File name.
+        reglog : list
+            List of records returned by :meth:`get_fit_record` 
+        
+        """
+        super().saveResults(outpath, fname, reglog=reglog)
+        
+        hasData = (self.data is not None)
+        hasFit = (self.fit is not None)
+        if (hasData and hasFit and fname):  
+            ss = self.formatIntensityModel()
+            if ss:
+                f = dataio.derive_filename(fname, ext='dat', sfx='imodel')
+                fn = str(dataio.get_output_file(f, path=outpath))            
+                print('Related intensity model saved in '+fn)
+                with open(fn, 'w') as f:
+                    f.write('# Fitted data: '+fname + "\n")
+                    f.write(ss)
+                    f.close
 
     def reportFit(self, outpath='', file='', reglog=None, **kwargs):
         """Report on the fit results.

stressfit/shapes/shapePlateCurved.py
@@ -55,11 +55,11 @@ class ShapePlateCurved(ShapeAbstract):
     def update(self, **kwargs):
         """Update parameters."""
         if 'thickness' in kwargs:
-            self.thickness = kwargs['thickness']
+            self.t = kwargs['thickness']
         if 'length' in kwargs:
-            self.size = np.array([kwargs['length'],self.height])
+            self.size = np.array([kwargs['length'],self.size[1]])
         if 'height' in kwargs:
-            self.size = np.array([self.length, kwargs['height']])
+            self.size = np.array([self.size[0], kwargs['height']])
         if 'rho1' in kwargs:
             self.rho1 = np.array(kwargs['rho1'])
         if 'rho2' in kwargs:

stressfit/ui/notebook.py
@@ -724,6 +724,9 @@ class UI_workspace(UI_base):
         self._values.update(self.wk.get_paths())
         self.update_widgets()
         self.update_values()
+        self._call_change()
+        # update workspace root for file widgets !
+        sw.set_workspace(self.wk.path(key='work', as_posix=True))
     
     def _on_path_change(self, inp):
         key = inp['name']
@@ -734,7 +737,11 @@ class UI_workspace(UI_base):
                 self.wk.change_workspace(root=value, create_tree=False, 
                                          save=False, verbose=True)            
             else:
-                self.wk.set_paths(**{key:value})
+                paths = {key:value}
+                # keep path to instruments equal to input data
+                if key == 'data':
+                    paths['instruments'] = value
+                self.wk.set_paths(**paths)
             self.set_from_wks()
     
     def _on_reset(self, b):
@@ -919,7 +926,7 @@ class UI_shape(UI_base):
             if isinstance(value, str):
                 wgt = sw.FileInput(name=key, 
                        file=value, 
-                       path=dataio.workspace().path('data'),
+                       path=dataio.workspace().full_path('data',as_posix=True),
                        #fileonly=True,
                        filetypes=(('Setup files','*.json')),
                        label=desc,
@@ -1481,7 +1488,15 @@ class UI_data(UI_base_list):
         else:
             fname = base
         return fname        
-        
+
+    def update_workspace(self, **kwargs):
+        """Update file input paths when workspace changed."""
+        fpath = self.wk.full_path('data', as_posix=True)
+        if 'intensity' in self._widgets:
+            self._widgets['intensity']._path = fpath
+        if 'strain' in self._widgets:
+            self._widgets['strain']._path = fpath
+
     def add_data(self, item, data=None, update_ui=False):
         """Add a dataset to the list.
         
@@ -1743,7 +1758,7 @@ class UI_plot_scene(UI_base):
                           logger=self._logger)
         self._widgets['proj'] = wdg 
         # plot range
-        wdg = ipy.IntSlider(value=self._values['rang'], min=3, max=50, step=1, 
+        wdg = ipy.IntSlider(value=self._values['rang'], min=3, max=100, step=1, 
                             description='Range: ')
         self._widgets['rang'] = wdg 
         
@@ -2026,6 +2041,11 @@ class UI_fit_distr(UI_base):
 
     """
     
+    def _raise_abs_class(fncname):
+        msg = 'UI_fit_distr is an abstract class.'
+        msg += 'Descendants must override {}.'
+        raise Exception(msg.format(fncname))
+
     def __init__(self, name):
         self._set_const()
         super().__init__(name, 
@@ -2043,8 +2063,31 @@ class UI_fit_distr(UI_base):
                             'loop': 0}
     
     def _set_const(self):
-        self._title = 'Fit intensity model'
-        self._cid = 'imodel'
+        """Set _title and _cid string constants.
+        
+        Abstract class, to be adapted by descendants.
+        
+        """
+        UI_fit_distr._raise_abs_class('_set_const')
+
+    def _create_fitobj(self, scan, model, scale, par):
+        """Create MCCfit object for given parameters.
+        
+        Abstract class, to be adapted by descendants.
+        
+        Parameters
+        ----------
+        par : dict
+            Command parameters returned by _uiconf.get_config(self.name)
+        scan : obj
+            Scan data returned by _uiconf.get_scan(par['data'])
+        model : obj
+            Model distribution returned by 
+            _uiconf.get_item(self._cid, item=par['model'])
+        scale : obj
+            Distribution scaling returned by model['scale']     
+        """
+        UI_fit_distr._raise_abs_class('_create_fitobj')
 
     def _create_widgets(self, **kwargs): 
 
@@ -2154,16 +2197,7 @@ class UI_fit_distr(UI_base):
         else:
             fname = base
         return fname
-    
-    def _create_fitobj(self, scan, model, scale, par):
-        fitobj = comm.define_ifit(scan, model['dist'], par['nrec'], 
-                                ndim=par['npts'])        
-        # define scaling parameters and interpolation model
-        fitobj.defScaling(scale['values'], scale['fit'], 
-                        minval=[0., 0., -np.inf])
-        fitobj.setInterpModel(model['interp'])
-        return fitobj
-    
+
     def _prepare(self):
         """Make all settings necessary to run commands.
         
@@ -2851,6 +2885,7 @@ class UI():
         self._last_input_file = 'input.json'
         self.ui = {}
         self.wk = dataio.workspace()
+        sw.set_workspace(self.wk.path(key='work', as_posix=True))
         msgl = ipy.Layout(width='100%', min_height='200px', border='none')
         self._note = ipy.Output(layout=ipy.Layout(width='100%', min_height='30px', border='none'))
         self._msg = ipy.Output(layout=msgl)
@@ -2880,6 +2915,7 @@ class UI():
         self._add_input_ui(UI_fit_emodel('fit_emodel'))
         
         # set event handlers
+        self.ui['workspace'].set_on_change(self._change)
         self.ui['shape'].set_on_change(self._change)
         self.ui['geometry'].set_on_change(self._change)
         self.ui['sampling'].set_on_change(self._change)
@@ -2963,6 +2999,8 @@ class UI():
             else:
                 if self.ui['shape'].select != 'File':
                     comm.set_shape(None,**kwargs)
+        if obj.name == 'workspace':
+            self.ui['data'].update_workspace(**kwargs)
         # update selection lists: geometry and sampling
         self._update_options(obj.name)
 
@@ -3099,6 +3137,8 @@ class UI():
                     # self.ui[key].notify()
                 except Exception:
                     self._exception('Cannot set values for {}:'.format(key))
+            # update shape data
+            self.ui['shape'].notify()
 
         except Exception:
             self._exception('Cannot load program configuration {}.'.format(filename))